7e933d7a213fff951b52a3a2ef31c2a34d44773b
Nayruden <nayruden@gmail.com>
2012-10-24 20:47:18 -0400

    Version bump since it's a a major change with GM13...

--- ulx/lua/ulx/sh_defines.lua
+++ ulx/lua/ulx/sh_defines.lua
@@ -1,6 +1,6 @@
 ulx.LOW_ARGS = "You did not specify enough arguments for this command. Type 'ulx help' in console for help."
 
-ulx.version = 3.55 -- Current release version. Don't access directly, use ulx.getVersion instead. (SVN checks)
+ulx.version = 3.60 -- Current release version. Don't access directly, use ulx.getVersion instead. (SVN checks)
 ulx.release = false -- Is this the release?
 
 ulx.ID_ORIGINAL = 1
